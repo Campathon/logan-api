ecc474c07d2f5873f3f20e7d5d89d0dce029fb1e
Max <tutv95@gmail.com>
2018-05-06 03:03:10 +0700

    test

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ io.on('connect', (socket) => {
     const {id} = socket;
     console.log('connected!', id);
 
-    console.log(socket.request);
+    console.log(socket.handshake);
 
     socket.on('disconnect', () => {
         console.log('disconnected!', id);
